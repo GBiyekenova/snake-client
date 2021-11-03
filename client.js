c084ee8bb5ad8c1f599497fd4eee8d096b44c931
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-03 18:15:31 +0000

    export to play.js

---
+++ client.js
@@ -0,0 +1,28 @@
+
+const net = require("net");
+
+const connect = () => {
+  // myFunction's code
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541// PORT number here,
+  });
+  conn.on("connect", () => {
+    console.log("yay, connection is made");// code that does something when the connection is first established
+  });
+  conn.on("data", () => {
+    console.log("you ded cuz you idled");// code that does something when the connection is first established
+  });
+  conn.on("close", () => {
+    console.log("closed connection");// code that does something when the connection is first established
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {
+  connect,// stores the function as connect
+};
