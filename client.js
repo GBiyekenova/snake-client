1e38ac5ba7cc458824b3d7009bb5ecc5a07031af
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-04 01:27:30 +0000

    transfer constants to a separate file

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
-
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = () => {
   // myFunction's code
   const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541// PORT number here,
+    host: IP,
+    port: PORT
   });
   conn.on("connect", () => {
     console.log("Successfully connected to game server");// code that does something when the connection is first established
