55864d8370c9fdce4db650e13887d547aaed69f6
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-04 00:30:20 +0000

    refactor

---
+++ input.js
@@ -0,0 +1,22 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+const handleUserInput = function (key) {
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+  console.log(key);
+  if (key === 'w') {
+    console.log('press w');
+  }
+  // your code here
+};
+
+module.exports = {setupInput}
