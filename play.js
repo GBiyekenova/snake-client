77c20e812cb34fb334aa35d9cf4587d41f63a2ef
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-04 00:31:07 +0000

    require input.js

--- play.js
+++ play.js
@@ -1,26 +1,5 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-const handleUserInput = function (key) {
-
-  if (key === '\u0003') {
-    process.exit();
-  }
-  console.log(key);
-  if (key === 'w') {
-    console.log('press w');
-  }
-  // your code here
-};
-
 setupInput();
