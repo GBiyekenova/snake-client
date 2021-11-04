6cdc258fd9fc2632750c427623bd4fd25c7c5445
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-04 00:09:19 +0000

    create a function handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
 const { connect } = require("./client");
 
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+const handleUserInput = function (key) {
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+  console.log(key);
+  if (key === 'w') {
+    console.log('press w');
+  }
+  // your code here
+};
+
+setupInput();
